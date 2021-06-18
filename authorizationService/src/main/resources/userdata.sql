3ee8517f3f7dce0ffda2116cdb07a53012dc89cf
CVamsi27 <55316723+CVamsi27@users.noreply.github.com>
2021-06-18 11:40:25 +0530

    Authorization microservice

---
+++ authorizationService/src/main/resources/userdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('101','John','abc123');
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('102','Divya','abc123');
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('103','Rishab','abc123');
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('104','Ram','abc123');
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('105','Venkat','abc123');
+
